90189a52ca54c99c4aeb5f8a6469e35cdaf7ad40
Kevin Stiward Balvin Garcia <kevin.balvin@globant.com>
2025-07-01 07:05:20 -0500

    feat variablesresidentevil

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,42 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        Personaje personaje = new Personaje();
 
 
+        System.out.println("***** RESIDENT EVIL CODE BY NO ONES");
+
+        //Personaje leon S. KJennedy
+        //Nombre
+        //Edad
+        //Cantidad de Vida
+
+        //Personake MR X (TITAN)
+        //Nombre
+        //virusqueloinfecto
+        //cantidadDeVida
+        final String nombrePersonaje;
+        final byte saludPersonaje;
+        byte Edad;
+
+        final String nombreZombie, tipoVirus;
+        byte saludZombie;
     }
+}
+
+class Personaje{
+    String nombre;
+    byte salud, Edad;
+
+    public void ataque(){
+        System.out.println("Posibles ataques: ");
+        System.out.println(" 1. cuchillo" +
+                " 2. Matilda");
+    }
+
 }
