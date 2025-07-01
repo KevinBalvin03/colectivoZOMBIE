12992a63d2e8e1a53d1fdd49cc1dca5e5e4696bc
Kevin Stiward Balvin Garcia <kevin.balvin@globant.com>
2025-07-01 09:05:33 -0500

    se configura clase scanner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner readData=new Scanner(System.in);
+        Scanner scanner =new Scanner(System.in);
 
         System.out.println("**** RESIDENT EVIL BY GLOBERS ****");
         System.out.println("DEVELOPER KBSG");
